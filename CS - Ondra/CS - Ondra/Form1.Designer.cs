8bb0d2b8a650b227f56d55128e23a17c5d75cb60
vomelaj.05 <vomelaj.05@A305-11>
2022-01-17 08:53:55 +0100

    Prvni uloženi

--- CS - Ondra/CS - Ondra/Form1.Designer.cs
+++ CS - Ondra/CS - Ondra/Form1.Designer.cs
@@ -28,20 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(186, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(35, 89);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(530, 264);
+            this.listBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(701, 493);
+            this.ClientSize = new System.Drawing.Size(605, 383);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- CS - Ondra/CS - Ondra/Form1.cs
+++ CS - Ondra/CS - Ondra/Form1.cs
@@ -16,5 +16,46 @@ namespace CS___Ondra
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            timer1.Tick += new EventHandler(program);
+        }
+
+        public void program(object sender, EventArgs e)
+        {
+            try
+            {
+                int vstup = int.Parse(textBox1.Text);
+                label1.Text = "";
+                if (vstup > 0)
+                {
+                    if (vstup > 9)
+                    {
+                        int cifSouc = 0;
+                        int pVstup = vstup;
+                        while (pVstup > 1)
+                        {
+                            int cifra = pVstup % 10;
+                            cifSouc += cifra;
+                            pVstup /= 10;
+                        }
+                        MessageBox.Show("Ciferný součin je " + cifSouc);
+                    }
+                    else
+                        MessageBox.Show("Ciferný součin je " + vstup);
+                }
+                else
+                {
+                    label1.Text = "Číslo není kladné!!";
+                    label1.ForeColor = Color.Red;
+                }
+            }
+            catch
+            {
+                label1.Text = "Neplatný formát";
+                label1.ForeColor = Color.Red;
+            }
+        }
     }
 }
